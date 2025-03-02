86b215446e6ca2b09f57401c0bfcdd79c4bb0fa2
Nayem5522 <sahidhasannayembd@gmail.com>
2025-03-02 10:52:54 +0600

    Update add_user_to_db.py

--- LazyDeveloper/add_user_to_db.py
+++ LazyDeveloper/add_user_to_db.py
@@ -1,17 +1,24 @@
-# in & as LazyDeveloper
-# Please Don't Remove Credit
 
 from configs import Config
 from handlers.database import db
 from pyrogram import Client
 from pyrogram.types import Message
+from pyrogram.errors import FloodWait
+import asyncio
 
 
 async def AddUserToDatabase(bot: Client, cmd: Message):
-    if not await db.is_user_exist(cmd.from_user.id):
-        await db.add_user(cmd.from_user.id)
-        if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
-                int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
-            )
+    user_id = cmd.from_user.id
+    if not await db.is_user_exist(user_id):
+        await db.add_user(user_id)
+
+        if Config.LOG_CHANNEL:
+            try:
+                await bot.send_message(
+                    int(Config.LOG_CHANNEL),
+                    f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={user_id}) started @{Config.BOT_USERNAME} !!"
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.value)
+            except Exception as err:
+                print(f"LOG_CHANNEL Error: {err}")
