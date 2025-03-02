5abf201a218b71a5aa021c1b3bcefdf9c0efcc55
Nayem5522 <sahidhasannayembd@gmail.com>
2025-03-02 10:53:45 +0600

    Update database.py

--- LazyDeveloper/database.py
+++ LazyDeveloper/database.py
@@ -1,5 +1,4 @@
-# in & as LazyDeveloper
-# Please Don't Remove Credit
+
 
 import datetime
 import motor.motor_asyncio
@@ -13,68 +12,56 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
 
-    def new_user(self, id):
+    def new_user(self, user_id):
         return dict(
-            id=id,
+            id=user_id,
             join_date=datetime.date.today().isoformat(),
             ban_status=dict(
                 is_banned=False,
                 ban_duration=0,
                 banned_on=datetime.date.max.isoformat(),
-                ban_reason=''
+                ban_reason=""
             )
         )
 
-    async def add_user(self, id):
-        user = self.new_user(id)
+    async def add_user(self, user_id):
+        user = self.new_user(user_id)
         await self.col.insert_one(user)
 
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
+    async def is_user_exist(self, user_id):
+        user = await self.col.find_one({"id": int(user_id)})
         return True if user else False
 
     async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
+        return await self.col.count_documents({})
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        users = await self.col.find({}, {"id": 1}).to_list(None)
+        return [user["id"] for user in users]
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
+        await self.col.delete_one({"id": int(user_id)})
 
-    async def remove_ban(self, id):
-        ban_status = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason=''
+    async def remove_ban(self, user_id):
+        await self.col.update_one(
+            {"id": int(user_id)}, {"$set": {"ban_status.is_banned": False}}
         )
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
 
     async def ban_user(self, user_id, ban_duration, ban_reason):
         ban_status = dict(
             is_banned=True,
             ban_duration=ban_duration,
             banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason
+            ban_reason=ban_reason,
         )
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
 
-    async def get_ban_status(self, id):
-        default = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason=''
-        )
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('ban_status', default)
+    async def get_ban_status(self, user_id):
+        user = await self.col.find_one({"id": int(user_id)})
+        return user.get("ban_status", {}) if user else {}
 
     async def get_all_banned_users(self):
-        banned_users = self.col.find({'ban_status.is_banned': True})
-        return banned_users
+        return self.col.find({"ban_status.is_banned": True})
 
 
 db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
